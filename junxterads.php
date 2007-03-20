d8d251130537f5a4234da44a08a351d728b1c0f7
xxxxusky <xxxxusky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-03-20 09:21:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/junxter-classifieds/trunk@8903 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ junxterads.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Plugin Name: JunxterAds
+Plugin URI: http://demo.junxter.com
+Description:Use this component to run Junxter Classifieds on your website. Your website will be able to display and post classified listings, allowing you to sell classified ad space on your website.  Users can find and post classified ads right from your website. 
+Author: Junxter Media
+Version: 0.1
+Author URI: http://www.junxter.com
+*/
+    
+
+function Junxter_main()
+{
+	$Pub_id ="";
+	$Style ="5";
+	
+	
+	if(!empty($Pub_id) && !empty($Style))
+	{
+		$key=$Pub_id ."x". $Style;
+    	}
+	
+	echo <<<emo
+<div class="pn-adminformrow">
+<SCRIPT LANGUAGE="javascript" TYPE="text/javascript"
+	SRC="http://zone.junxter.com/$key">
+</SCRIPT>
+</div>
+emo;
+    
+
+}
+
+
+
+?>
---
+++ wp-junxter.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Junxter Ads
+*/
+?>
+
+<?php get_header();?>
+
+<?php
+$Pub_id = "ee4045890fd0b0ebce52d4cf45393ab0-6";
+
+echo <<<emo
+
+    <div id=junxter-adStation>
+    <table width=95% style='margin:0 auto;background:#fff'><td>
+    <link rel="stylesheet" type="text/css"
+      href="http://r.junxter.com/css/style.css" />
+    <script language="javascript"
+      src="http://r.junxter.com/$Pub_id">
+    </script>
+    </td></table></div>
+
+emo;
+?>
+
+
+
+<?php get_footer(); ?>
